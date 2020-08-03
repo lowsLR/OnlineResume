270affe440c28eb2a343da064a55607b4af863bb
lowsLR <1228747929@qq.com>
2020-08-03 17:36:26 +0800

    记录

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,7 +3,12 @@
 		<!-- 声明 -->
 		<view class="statement">
 			我是一个前端小白，插件这么牛逼的事情本来与我无关的，但奈何前端大佬说这是一个小任务，写一个uni插件，嗯……想了很久，终于看到一个有趣的超真实手绘风格 UI 元素集合
-			<wired-link elevation="2" href="https://github.com/rough-stuff/wired-elements/tree/master/packages/wired-link" target="_blank" style="color: #F59;">
+			<wired-link
+				elevation="3"
+				href="https://github.com/rough-stuff/wired-elements/tree/master/packages/wired-link"
+				target="_blank"
+				style="color: #F59; width: auto;overflow: hidden;vertical-align:top;"
+			>
 				'wired-elements'
 			</wired-link>
 			的简易插件，我就用这个写一个在线简历，对于刚出来找工作的新人来说，有一份在线简历也算是一份门面，毕竟人靠衣装，佛靠金装嘛。如果觉得不好看的，大佬们别喷。
@@ -12,7 +17,7 @@
 		<view class="toUse">
 			<view><wired-link elevation="1" href="https://wiredjs.com/showcase.html" target="_blank">组件库展示</wired-link></view>
 			<view>
-				<wired-listbox horizontal selected="One"><wired-item value="One">在线简历</wired-item></wired-listbox>
+				<wired-listbox horizontal selected="One" class="customListBox"><wired-item value="One" @click="resume">在线简历</wired-item></wired-listbox>
 			</view>
 		</view>
 	</view>
@@ -26,24 +31,34 @@ export default {
 		};
 	},
 	onLoad() {},
-	methods: {}
+	methods: {
+		resume(){
+			uni.navigateTo({
+				url:'../resume/resume'
+			})
+		}
+	}
 };
 </script>
 
 <style scoped lang="scss">
+.content {
+	font-family: sans-serif;
+}
 .statement {
 	padding: 0 40rpx;
-	font-family: inherit;
 	font-size: 28rpx;
 	line-height: 50rpx;
 	text-indent: 2em;
 }
-/deep/.wired-rendered {
+.wired-rendered {
 	text-indent: 0;
-	.selected {
-		color: red;
-	}
 }
+// .customListBox {
+// 	--wired-item-selected-bg: #f59;
+// 	--wired-item-selected-color: #fff;
+// 	// width: auto;
+// }
 .toUse {
 	margin-top: 40rpx;
 	display: flex;
---
+++ pages/resume/resume.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>xxxxxx</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
