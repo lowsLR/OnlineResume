4b9e965b07e4b45951892176676fe5858c4976e2
lowsLR <1228747929@qq.com>
2020-08-12 18:00:58 +0800

    8.12

---
+++ component/resume-header/resume-header.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="resume-header">
+		<view class="brief-introduction">
+			<view class="brief-l">xxx</view>
+			<view class="brief-img"><image src="../../static/logo.png" mode=""></image></view>
+			<view class="brief-r">xxx</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'resume-header'
+};
+</script>
+
+<style scoped>
+.brief-introduction {
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border: 1px solid red;
+	min-height: 400rpx;
+}
+.brief-introduction .brief-img {
+	width: 160rpx;
+	height: 160rpx;
+	border: 1rpx solid;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 5rpx;
+	border-radius: 50%;
+	overflow: hidden;
+	margin: 0 30rpx;
+}
+.brief-introduction .brief-img image {
+	width: 100%;
+	height: 100%;
+	display: block;
+	border-radius: 50%;
+}
+</style>
--- pages/resume/resume.vue
+++ pages/resume/resume.vue
@@ -1,9 +1,44 @@
 <template>
-	<view>xxxxxx</view>
+	<view class="resume-layout">
+		<!-- 背景 -->
+		<wired-card elevation="4" :fill="fill" class="resume-bg" :style="{ backgroundImage: linearGradient }">
+			<!-- 简介 -->
+			<resume-header></resume-header>
+		</wired-card>
+
+		<!-- 经验 -->
+		<!-- 作品/项目-->
+		<!-- 联系 -->
+		<!-- 尾部 -->
+	</view>
 </template>
 
 <script>
+import resumeHeader from '../../component/resume-header/resume-header.vue';
+export default {
+	name: 'resume',
+	components: {
+		resumeHeader
+	},
+	data() {
+		return {
+			fill: '#FFFFFF',
+			linearGradient: '(to top left, #b1b1b1, #f2f2f2)'
+		};
+	}
+};
 </script>
 
-<style>
+<style scoped>
+* {
+	margin: 0;
+	padding: 0;
+}
+.resume-bg {
+	color: #333;
+	width: 100%;
+	min-height: 1200rpx;
+	/* background: #333; */
+	background-image: linear-gradient(to top left, #b1b1b1, #f2f2f2);
+}
 </style>
