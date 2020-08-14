25cadfa0bb49fca852f1e58304c0559853132c9e
lowsLR <1228747929@qq.com>
2020-08-14 10:56:23 +0800

    8.14

--- component/resume-experience/resume-experience.vue
+++ component/resume-experience/resume-experience.vue
@@ -1,21 +1,23 @@
 <template>
 	<view class="resume-experience">
 		<view class="line"><wired-divider class="lineColor" :style="{ color: lineColor }"></wired-divider></view>
-		<swiper :indicator-dots="indicatorDots" :autoplay="false" class="swiper-layout" :indicator-active-color="indictor">
-			<swiper-item class="swiper-item-container" v-for="(item,index) in experienceArr" :key="index">
+		<swiper :indicator-dots="indicatorDots" :autoplay="false" :indicator-active-color="indictor" :style="{height:swiperHeight}" @change="changeSwiperPage">
+			<swiper-item  v-for="(item, index) in experienceArr" :key="index">
 				<view class="swiper-item">
-					<view class="corporate">
-						<view class="name">{{item.exName}}</view>
-						<wired-image
-							:elevation="index >= 5?5:index"
-							:src="item.exSrc"
-							class="swiper-img"
-							:style="{ color: exImageColor, width: exWidth, height: xeHeight }"
-						></wired-image>
+					<view class="name">{{ item.exName }}</view>
+					<view class="corporate-container">
+						<view class="corporate">
+							<wired-image
+								:elevation="index >= 5 ? 5 : index"
+								:src="item.exSrc"
+								class="swiper-img"
+								:style="{ color: exImageColor, width: exWidth, height: xeHeight }"
+							></wired-image>
+						</view>
+						<view class="swiper-content">
+							<view v-for="(items, indexs) in item.exsContent" :key="indexs" class="exText">{{ indexs + 1 }}. {{ items.text }}</view>
+						</view>
 					</view>
-					<view class="swiper-content">
-						<view v-for="(items,indexs) in item.exsContent" :key="indexs">{{indexs+1}}. {{items.text}}</view>
-					</view>	
 				</view>
 			</swiper-item>
 		</swiper>
@@ -33,32 +35,41 @@ export default {
 			default: () => {
 				return [
 					{
-						exName:'广州xxxx信息有限公司1',
-						exSrc:'https://www.gstatic.com/webp/gallery/1.sm.jpg',
-						exsContent:[
+						exName: '广州xxxx信息有限公司1',
+						exSrc: 'https://www.gstatic.com/webp/gallery/1.sm.jpg',
+						exsContent: [
 							{
-								text:'根据UI设计还原页面；'
+								text: '根据UI设计还原页面；'
 							},
 							{
-								text:'与后端交互数据；'
+								text: '与后端交互数据；'
 							},
 							{
-								text:'负责公司的官网和微信公众号开发；'
+								text: '负责公司的官网和微信公众号开发；'
 							}
 						]
 					},
 					{
-						exName:'广州xxxx信息有限公司2',
-						exSrc:'https://www.gstatic.com/webp/gallery/2.sm.jpg',
-						exsContent:[
+						exName: '广州xxxx信息有限公司2',
+						exSrc: 'https://www.gstatic.com/webp/gallery/2.sm.jpg',
+						exsContent: [
 							{
-								text:'根据UI设计还原页面；'
+								text: '根据UI设计还原页面；'
 							},
 							{
-								text:'与后端交互数据；'
+								text: '与后端交互数据；'
 							},
 							{
-								text:'负责公司的官网和微信公众号开发；'
+								text: '负责公司的官网和微信公众号开发；'
+							},
+							{
+								text: '根据UI设计还原页面1；'
+							},
+							{
+								text: '与后端交互数据1；'
+							},
+							{
+								text: '负责公司的官网和微信公众号开发1；'
 							}
 						]
 					}
@@ -88,8 +99,8 @@ export default {
 			default: () => {
 				return '#B1B1B1';
 			}
-		},	
-		indictor:{
+		},
+		indictor: {
 			type: String,
 			default: () => {
 				return '#FFFFFF';
@@ -99,7 +110,9 @@ export default {
 	data() {
 		return {
 			array: [],
-			indicatorDots: false
+			indicatorDots: false,
+			swiperHeight: '',
+			changeIndex:0
 		};
 	},
 	created() {
@@ -109,43 +122,75 @@ export default {
 			if (_self.array.length > 1) {
 				_self.indicatorDots = true;
 			}
+			_self.getElementHeight('.exText',_self.changeIndex);
 		});
+	},
+	methods: {
+		getElementHeight(element,changeIndex) {
+			setTimeout(() => {
+				let query = uni.createSelectorQuery().in(this);
+				query.select(element).boundingClientRect();
+				query.exec(res => {
+					console.log(res,"==>res");
+					if (!res) {
+						//如果没获取到，再调一次
+						_self.getElementHeight();
+					} else {
+						// _self.swiperHeight = res[0].height+'px';
+					_self.swiperHeight = _self.array[changeIndex].exsContent.length*res[0].height+120+'px';
+						console.log(_self.swiperHeight);
+					}
+				});
+			}, 20);
+		},
+		changeSwiperPage(e){
+			_self.changeIndex = e.detail.current;
+			_self.getElementHeight('.exText',_self.changeIndex);
+		}
 	}
 };
 </script>
 
 <style scoped>
+page {
+	width: 100%;
+	min-height: 100%;
+	display: flex;
+}
 .resume-experience {
 	/* border: 1px solid red; */
 	margin-top: 20rpx;
 }
 .swiper-layout {
-	min-height: 400rpx;
 	padding: 0 10rpx;
-}
-.swiper-item-container {
-	display: flex;
-	align-items: center;
 }
 .swiper-item {
 	display: flex;
-	align-items: center;
+	flex-direction: column;
 }
-.corporate .name {
-	width: 240rpx;
+.swiper-item .name {
+	width: 100%;
+	text-align: center;
+	font-weight: bold;
+	font-size: 36rpx;
+	margin-bottom: 40rpx;
+}
+.corporate-container {
 	display: flex;
 	align-items: center;
-	justify-content: center;
-	font-size: 28rpx;
-	font-weight: bold;
+	/* border: 1px solid red; */
 }
 .swiper-img {
+	width: 240rpx;
 	display: flex;
 	margin-right: 20rpx;
 }
 .swiper-content {
 	flex: 1;
 	font-size: 32rpx;
+}
+.swiper-content view:nth-child(2n) {
+	margin: 10rpx 0;
 }
 .line {
 	margin: 10rpx 0;
