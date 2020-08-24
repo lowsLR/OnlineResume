bf95110e2bd3429354de950746025da1087f61de
lowsLR <1228747929@qq.com>
2020-08-24 21:14:10 +0800

    8.24

--- pages/establish/establish.vue
+++ pages/establish/establish.vue
@@ -1,42 +1,52 @@
 <template>
 	<view class="establish-layout">
-		<view class="resume-establish">
-			<!-- 1.是否自定义主题，背景颜色，渐变色，字体颜色，若选择默认下面的2，3点不显示 -->
-			<!-- 2.是否自定基本信息背景色 -->
-			<!-- 3.是否自定义线条颜色 -->
-			<!-- 4.上传头像 ***-->
-			<!-- 5.简介标题/内容填写 -->
-			<!-- 6.经验填写 -->
-			<!-- 7.项目填写 -->
-			<!-- 8.联系填写 -->
-			<!-- 9.尾部结束语填写-以及是否显示版权 -->
+		<!-- 1.是否自定义主题，背景颜色，渐变色，字体颜色，若选择默认下面的2，3点不显示 -->
+		<!-- 2.是否自定基本信息背景色 -->
+		<!-- 3.是否自定义线条颜色 -->
+		<!-- 4.上传头像 ***-->
+		<!-- 5.简介标题/内容填写 -->
+		<!-- 6.经验填写 -->
+		<!-- 7.项目填写 -->
+		<!-- 8.联系填写 -->
+		<!-- 9.尾部结束语填写-以及是否显示版权 -->
 
-			<!-- 需要解决的问题，1.from表单，2.图片上传，3.动态添加多个input框 -->
-			<form @submit="formSubmit" @reset="formReset">
-				<view class="uni-switch">
-					<view class="switch-title">是否自定义主题颜色</view>
-					<view class="toggle-switch"></view>
-					<wired-toggle :checked="themeChecked" @change="themeFun(themeChecked)"></wired-toggle>
-					<switch name="switchTheme" :checked="themeChecked" v-show="false" />
-				</view>
-				<view class="uni-switch" v-if="themeChecked">
-					<view class="switch-title">是否自定基本信息背景色</view>
-					<view class="toggle-switch"></view>
-					<wired-toggle :checked="infoChecked" @change="infoFun(infoChecked)"></wired-toggle>
-					<switch name="switchInfo" :checked="infoChecked" v-show="false" />
-				</view>
-				<view class="uni-switch" v-if="themeChecked">
-					<view class="switch-title">是否自定义线条颜色</view>
-					<view class="toggle-switch"></view>
-					<wired-toggle :checked="lineChecked" @change="lineFun(lineChecked)"></wired-toggle>
-					<switch name="switchLine" :checked="lineChecked" v-show="false" />
-				</view>
-				<view class="uni-btn-v">
-					<button form-type="submit"><wired-button class="wired-button">提交</wired-button></button>
-					<button type="default" form-type="reset"><wired-button elevation="3" class="wired-button">重置</wired-button></button>
-				</view>
-			</form>
-		</view>
+		<!-- 需要解决的问题，1.from表单，2.图片上传，3.动态添加多个input框 -->
+		<wired-card elevation="3" class="card-form">
+			<view class="resume-establish">
+				<form @submit="formSubmit" @reset="formReset">
+					<!-- 是否自定义 -->
+					<view class="uni-switch">
+						<view class="switch-title">是否自定义主题颜色</view>
+						<view class="toggle-switch"></view>
+						<wired-toggle :checked="themeChecked" @change="themeFun(themeChecked)"></wired-toggle>
+						<switch name="switchTheme" :checked="themeChecked" v-show="false" />
+					</view>
+					<view class="uni-switch" v-if="themeChecked">
+						<view class="switch-title">是否自定基本信息背景色</view>
+						<view class="toggle-switch"></view>
+						<wired-toggle :checked="infoChecked" @change="infoFun(infoChecked)" class="infoColor"></wired-toggle>
+						<switch name="switchInfo" :checked="infoChecked" v-show="false" />
+					</view>
+					<view class="uni-switch" v-if="themeChecked">
+						<view class="switch-title">是否自定义线条颜色</view>
+						<view class="toggle-switch"></view>
+						<wired-toggle :checked="lineChecked" @change="lineFun(lineChecked)" class="lineColor"></wired-toggle>
+						<switch name="switchLine" :checked="lineChecked" v-show="false" />
+					</view>
+					<!-- 上传头像 -->
+					<view class="chooseImage-header">
+						<view @click="chooseImageH">上传图片</view>
+						<image :src="imgSrc" mode="" v-if="imgSrc"></image>
+					</view>
+					<!-- 性别 -->
+					<!-- 提交 -->
+					<view class="uni-btn-v">
+						<button form-type="submit"><wired-button class="wired-button">提交</wired-button></button>
+						<button type="default" form-type="reset"><wired-button elevation="3" class="wired-button">重置</wired-button></button>
+					</view>
+				</form>
+			</view>
+		</wired-card>
 	</view>
 </template>
 
@@ -48,7 +58,8 @@ export default {
 		return {
 			themeChecked: false,
 			infoChecked: false,
-			lineChecked: false
+			lineChecked: false,
+			imgSrc: ''
 		};
 	},
 	onLoad() {
@@ -69,15 +80,43 @@ export default {
 		},
 		lineFun(e) {
 			_self.lineChecked = !_self.lineChecked;
+		},
+		chooseImageH() {
+			uni.chooseImage({
+				count: 1, //默认9
+				sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+				sourceType: ['album'], //从相册选择
+				success: function(res) {
+					console.log(JSON.stringify(res.tempFilePaths));
+					uni.getImageInfo({
+						src: res.tempFilePaths[0],
+						success: function(image) {
+							console.log(image.width);
+							console.log(image);
+							_self.imgSrc = image.path;
+						}
+					});
+				}
+			});
 		}
 	}
 };
 </script>
 
 <style scoped>
+* {
+	margin: 0;
+	padding: 0;
+}
 .establish-layout {
-	padding: 10rpx;
 	font-size: 36rpx;
+	width: 100%;
+}
+.card-form {
+	width: 100%;
+}
+.resume-establish {
+	padding: 20rpx;
 }
 .uni-btn-v {
 	display: flex;
@@ -86,7 +125,6 @@ export default {
 }
 .uni-btn-v button {
 	background-color: transparent;
-	/* border: 1px solid red; */
 }
 .uni-btn-v button::after {
 	outline: none;
@@ -102,5 +140,13 @@ export default {
 .toggle-switch {
 	display: flex;
 	align-items: center;
+}
+.infoColor {
+	--wired-toggle-off-color: red;
+	--wired-toggle-on-color: green;
+}
+.lineColor {
+	--wired-toggle-off-color: #f59;
+	--wired-toggle-on-color: #f50;
 }
 </style>
